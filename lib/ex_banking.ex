7454867907e9edc9eec2e73f2ccd3e30556bddb3
pau-riosa <jethro.riosa@gmail.com>
2020-09-21 18:53:51 +0800

    working in progress get_balance and also rounding off to decimal places

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -23,7 +23,7 @@ defmodule ExBanking do
   """
   @spec create_user(user :: String.t()) :: :ok | banking_error
   def create_user(user) do
-    with [] <- :ets.lookup(:ex_banking, String.to_atom(user)),
+    with [] <- lookup(user),
          {:ok, created_user} <- User.add_user(user) do
       :ets.insert(:ex_banking, {String.to_atom(user), created_user})
       :ok
@@ -43,17 +43,12 @@ defmodule ExBanking do
          {:ok, cross_rate} <- Money.cross_rate(state.money, currency),
          {:ok, deposit_money} <- Money.mult(Money.new(state.money.currency, 1), cross_rate),
          {:ok, current_money} <- Money.add(state.money, deposit_money) do
-      IO.inspect(current_money)
       new_state = %{state | money: current_money}
       :ets.insert(:ex_banking, {name, new_state})
-      {:ok, current_money}
+      {:ok, current_money.amount}
     else
       _ -> {:error, :user_does_not_exist}
     end
-  end
-
-  def lookup(user) do
-    :ets.lookup(:ex_banking, String.to_atom(user))
   end
 
   @doc """
@@ -63,7 +58,19 @@ defmodule ExBanking do
   @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
           {:ok, new_balance :: number} | banking_error
   def withdraw(user, amount, currency) do
-    IO.inspect(:withdraw)
+    with [{name, state}] <- lookup(user),
+         {:ok, cross_rate} <- Money.cross_rate(state.money, currency),
+         {:ok, withdraw_money} <- Money.mult(Money.new(state.money.currency, 1), cross_rate),
+         :gt <-
+           Money.compare(state.money, Money.new(state.money.currency, 0)),
+         {:ok, current_money} <- Money.sub(state.money, withdraw_money) do
+      new_state = %{state | money: Money.abs(current_money)}
+      :ets.insert(:ex_banking, {name, new_state})
+      {:ok, Money.abs(current_money).amount}
+    else
+      :eq -> {:error, :not_enough_money}
+      _ -> {:error, :user_does_not_exist}
+    end
   end
 
   @doc """
@@ -72,7 +79,13 @@ defmodule ExBanking do
   @spec get_balance(user :: String.t(), currency :: String.t()) ::
           {:ok, balance :: number} | banking_error
   def get_balance(user, currency) do
-    IO.inspect(:get_balance)
+    with [{key, state}] <- lookup(user),
+         {:ok, cross_rate} <- Money.cross_rate(state.money, currency),
+         {:ok, current_money} <- Money.mult(state.money, cross_rate) do
+      {:ok, current_money}
+    else
+      [] -> {:error, :user_does_not_exist}
+    end
   end
 
   @doc """
@@ -89,5 +102,9 @@ defmodule ExBanking do
 
   def send(from_user, to_user, amount, currency) do
     IO.inspect(:send)
+  end
+
+  def lookup(user) do
+    :ets.lookup(:ex_banking, String.to_atom(user))
   end
 end
